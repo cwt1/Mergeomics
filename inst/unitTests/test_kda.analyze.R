f0bdcb71fdfebb4b4dd4b2de9f86a9898269afde
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-10-17 19:00:47 +0000

    Creating branch for BioC 3.4 release
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_4/madman/Rpacks/Mergeomics@122711 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_kda.analyze.R
@@ -0,0 +1,38 @@
+test_kda.analyze <- function() {
+    library(RUnit)
+    library(Mergeomics)
+
+    job.kda <- list()
+    job.kda$label<-"HDLC"
+    ## parent folder for results
+    job.kda$folder<-"Results"
+    ## Input a network
+    ## columns: TAIL HEAD WEIGHT
+    job.kda$netfile<-system.file("extdata","network.mouseliver.mouse.txt", 
+    package="Mergeomics")
+    ## Gene sets derived from ModuleMerge, containing two columns, MODULE, 
+    ## NODE, delimited by tab 
+    job.kda$modfile<- system.file("extdata","mergedModules.txt", 
+    package="Mergeomics")
+    ## "0" means we do not consider edge weights while 1 is opposite.
+    job.kda$edgefactor<-0.0
+    ## The searching depth for the KDA
+    job.kda$depth<-1
+    ## 0 means we do not consider the directions of the regulatory 
+    ## interactions; while 1 is opposite.
+    job.kda$direction <- 1
+    job.kda$nperm <- 100 # the default value is 2000, use 100 for unit tests
+    ## Let's run KDA!
+    job.kda <- kda.configure(job.kda)
+    job.kda <- kda.start(job.kda)
+    job.kda <- kda.prepare(job.kda)
+    job.kda <- kda.analyze(job.kda)
+    
+    ## compare the pvals with the expected ones:
+    ## since we set the seed for random # generation, we know the exact
+    ## results for our input sets:
+   
+    ## check Pvalues of the top KD
+    checkEqualsNumeric(as.numeric(sort(job.kda$results$P))[1], 
+    1.610e-68, tolerance=1.0e-4)
+}
