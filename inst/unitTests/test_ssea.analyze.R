fa8bbcc64a293221c1190c183a67ad881bc38ff1
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-24 19:45:44 +0000

    Creating branch for BioC 3.5 release
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/Mergeomics@129128 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_ssea.analyze.R
@@ -0,0 +1,29 @@
+test_ssea.analyze <- function() {
+    library(RUnit)
+    library(Mergeomics)
+    job.msea <- list()
+    job.msea$label <- "hdlc"
+    job.msea$folder <- "Results"
+    job.msea$genfile <- system.file("extdata", 
+    "genes.hdlc_040kb_ld70.human_eliminated.txt", package="Mergeomics")
+    job.msea$marfile <- system.file("extdata", 
+    "marker.hdlc_040kb_ld70.human_eliminated.txt", package="Mergeomics")
+    job.msea$modfile <- system.file("extdata", 
+    "modules.mousecoexpr.liver.human.txt", package="Mergeomics")
+    job.msea$inffile <- system.file("extdata", 
+    "coexpr.info.txt", package="Mergeomics")
+    job.msea$nperm <- 100 ## default value is 20000
+    
+    ## run ssea.start() 
+    job.msea <- ssea.start(job.msea)
+    job.msea <- ssea.prepare(job.msea)
+    job.msea <- ssea.control(job.msea)
+    job.msea <- ssea.analyze(job.msea)
+    
+    ## compare the pvals with the expected ones:
+    ## since we set the seed for random # generation, we know the exact
+    ## results for our input sets:
+    checkEqualsNumeric(sort(as.numeric(job.msea$results$P))[2], 
+    2.25e-60, tolerance=1.0e-4)
+
+}
